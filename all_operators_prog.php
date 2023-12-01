127e4d1aa171162a26ede555728f796a2f272ace
Vaja <vajadipak968@gmail.com>
2023-12-01 16:58:45 +0530

    01/12/23

---
+++ all_operators_prog.php
@@ -0,0 +1,21 @@
+<li>Arithmetic Operators (+,-,*,/,**,%)</li>
+<li>Logical or Relational Operators (and,or,xor,&&,||,!)</li>
+<li>Comparison Operators (==,===,!=,<>,!==,>,<,>=,<=,<=>)</li>
+<li>Conditional or Ternary Operators (?:,??)</li>
+<li>Assignment Operators (=,+=,-=,*=,/=,%=)</li>
+<li>Spaceship Operators (Introduced in PHP 7) (<=>)</li>
+<li>Array Operators (+,==,===,!=,<>,!==)</li>
+<li>String Operators (.,.=)</li>
+<li>Increment/Decrement Operators (++$variable, $variable++, --$variable, $variable--)</li>
+
+<?php
+
+$a = 23;
+$b = 13;
+$c = 57;
+
+echo "<br>=========== Arithmetic Operators ========== <br>";
+
+echo "Sum of $a and $b = " . $a + $b . "<br>";
+
+?>
