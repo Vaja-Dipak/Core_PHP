6171352284228a046a252fb63ee48c6b31c1f8c3
Vaja Dipak <91632488+Vaja-Dipak@users.noreply.github.com>
2023-12-07 19:01:00 +0530

    Update 15Interface.php

--- practice_Data/PHP/18OOPS/15Interface.php
+++ practice_Data/PHP/18OOPS/15Interface.php
@@ -1,5 +1,12 @@
 <?php
 
+// Interface are 100% abstraction class
+// An interface is declared using the "interface" keyword.
+// A class can implement multiple interfaces using "implement" keyword
+// All Interfaces are must be impliment compulsory.
+// Interfaces can't maintain Non-abstract methods.
+// An interface can define method names and arguments, but not the contents of the methods or any other. 
+
 interface a{
     // function test(){
     //     echo "called";
